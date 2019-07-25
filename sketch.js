451cd54960b1b6fd7cfe758dd17bb8066203dbd9
AlphaCodingcode <dilshan@alphacoding.com>
2019-07-25 10:54:17 -0400

    init

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// Any global variables can be defined up here
+
+function preload() {
+
+}
+
+function setup() {
+    createCanvas(width, height);
+}
+
+function draw() {
+    // Update
+
+    // Render
+
+}
